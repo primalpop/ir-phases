1bdd40422f3a2b21e03055ac49e10181b41c79bc
primalpop <primal007@gmail.com>
2014-04-10 14:11:37 -0400

    changed to dictionaries; to test query results. First version of the report

---
+++ 4thPhase/test.sh
@@ -0,0 +1,12 @@
+program=$1
+in=$2
+out=$3
+exp_len=$4
+
+#usage: ./test.sh ./index ../input_files out_files "100 200 300 400 500"
+
+echo "" > times.csv
+for i in $exp_len
+do
+  /usr/bin/time "$program" $in $out $i 2>> times.csv
+done
